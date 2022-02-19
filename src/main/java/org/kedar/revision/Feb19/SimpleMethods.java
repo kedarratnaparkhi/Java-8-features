39dcb44ea251fffc32b56eb25caed4d68bcfcf59
kedar <kedar.ratnaparkhi.dev@gmail.com>
2022-02-19 18:04:13 +0000

    added .gitignore

--- src/main/java/org/kedar/revision/Feb19/SimpleMethods.java
+++ src/main/java/org/kedar/revision/Feb19/SimpleMethods.java
@@ -3,11 +3,15 @@ package org.kedar.revision.Feb19;
 import org.kedar.extras.Employee;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SimpleMethods {
     public static void main(String[] args) {
         List<Employee> employees = Employee.listOfEmployees;
 
-        employees.stream().filter(emp -> emp.getAge() < 25).forEach(System.out::println);
+        List<Double> ages = employees.stream().mapToDouble(emp -> emp.getAge())
+                .mapToObj(Double::valueOf).collect(Collectors.toList());
+
+        System.out.println(ages);
     }
 }
